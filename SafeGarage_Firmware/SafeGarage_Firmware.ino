15116e0d6164d8eff7d9f0de29410be8a307b72c
Lucas <lukegauldin93@gmail.com>
2019-12-06 09:04:21 -0500

    Initial Commit
    Created Projects for mobile app and firmware

---
+++ SafeGarage_Firmware/SafeGarage_Firmware.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SafeGarage_Firmware.ino
+ Created:	12/6/2019 9:02:38 AM
+ Author:	lukeg
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ SafeGarage_Mobile/app/src/main/java/com/quickreports/safegarage_mobile/viewmodels/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.quickreports.safegarage_mobile.viewmodels;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
