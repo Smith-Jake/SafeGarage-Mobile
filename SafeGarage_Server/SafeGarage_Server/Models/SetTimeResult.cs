4c87dd0926ad7c5b9e7350099b9fd05c03ad60c1
Lucas <lukegauldin93@gmail.com>
2019-12-30 12:44:05 -0500

    Added base classes for REST API
    TODO: make functions callable from web

---
+++ SafeGarage_Server/SafeGarage_Server/Models/SetTimeResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SafeGarage_Server.Models
+{
+    public class SetTimeResult : BaseModel
+    {
+        [JsonProperty(PropertyName = "success")]
+        public bool Success { get; set; }
+
+        [JsonProperty(PropertyName = "close_time")]
+        public DateTime CloseTime { get; set; }
+    }
+}
---
+++ SafeGarage_Server/SafeGarage_Server/ServiceProvider/ServiceProvider_ToggleGarageDoor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SafeGarage_Server.Models;
+
+namespace SafeGarage_Server.ServiceProvider
+{
+    public partial class ServiceProvider
+    {
+        public ServiceResponse<ToggleResult> ToggleGarageDoor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
