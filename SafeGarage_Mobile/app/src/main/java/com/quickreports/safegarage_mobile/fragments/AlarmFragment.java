a37a0ba87db8917ff68295d8648ca15e63b2c523
Jake <smith.2198@wright.edu>
2020-01-16 16:52:31 -0500

    added the ability to swipe to previous/next top fragments

--- SafeGarage_Mobile/app/src/main/java/com/quickreports/safegarage_mobile/fragments/AlarmFragment.java
+++ SafeGarage_Mobile/app/src/main/java/com/quickreports/safegarage_mobile/fragments/AlarmFragment.java
@@ -16,22 +16,13 @@ import com.quickreports.safegarage_mobile.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AlarmFragment.OnFragmentInteractionListener} interface
+ * {@link AlarmFragment.OnAlarmFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link AlarmFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class AlarmFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private OnAlarmFragmentInteractionListener mListener;
 
     public AlarmFragment() {
         // Required empty public constructor
@@ -41,27 +32,16 @@ public class AlarmFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AlarmFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static AlarmFragment newInstance(String param1, String param2) {
+    public static AlarmFragment newInstance() {
         AlarmFragment fragment = new AlarmFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -74,15 +54,15 @@ public class AlarmFragment extends Fragment {
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onAlarmFragmentInteraction(uri);
         }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnAlarmFragmentInteractionListener) {
+            mListener = (OnAlarmFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -105,8 +85,8 @@ public class AlarmFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface OnAlarmFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onAlarmFragmentInteraction(Uri uri);
     }
 }
--- SafeGarage_Mobile/app/src/main/java/com/quickreports/safegarage_mobile/fragments/TemperatureFragment.java
+++ SafeGarage_Mobile/app/src/main/java/com/quickreports/safegarage_mobile/fragments/TemperatureFragment.java
@@ -16,22 +16,14 @@ import com.quickreports.safegarage_mobile.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link TemperatureFragment.OnFragmentInteractionListener} interface
+ * {@link TemperatureFragment.OnTemperatureFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link TemperatureFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class TemperatureFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private OnTemperatureFragmentInteractionListener mListener;
 
     public TemperatureFragment() {
         // Required empty public constructor
@@ -41,27 +33,16 @@ public class TemperatureFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment TemperatureFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static TemperatureFragment newInstance(String param1, String param2) {
+    public static TemperatureFragment newInstance() {
         TemperatureFragment fragment = new TemperatureFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -74,15 +55,15 @@ public class TemperatureFragment extends Fragment {
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onTemperatureFragmentInteraction(uri);
         }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnTemperatureFragmentInteractionListener) {
+            mListener = (OnTemperatureFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -105,8 +86,8 @@ public class TemperatureFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface OnTemperatureFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onTemperatureFragmentInteraction(Uri uri);
     }
 }
