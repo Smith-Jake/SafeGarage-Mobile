20c23ace164e77f158e9326c72ac0077b9a3b714
Lucas <lukegauldin93@gmail.com>
2020-03-10 10:30:51 -0400

    added REST API index page

---
+++ SafeGarage_Server/SafeGarage_Server/Models/PageModels/IndexModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SafeGarage_Server.Models.PageModels
+{
+    public class IndexModel
+    {
+    }
+}
